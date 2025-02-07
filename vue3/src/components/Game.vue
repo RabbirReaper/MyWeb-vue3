a28dae3d5eb8872285865d72dbc8063e2a457bc2
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-02-07 16:17:19 +0800

    test bcrypt

--- vue3/src/components/Game.vue
+++ vue3/src/components/Game.vue
@@ -1,133 +1,64 @@
 <script setup>
 import { ref } from 'vue'
-// import fs from 'fs'
+import bcrypt from 'bcryptjs'
 
-const x = ref([7, 2, 3, 4, 5])
+// 定義初始密碼和輸入密碼
+const storedHashedPassword = ref('') // 儲存加密的密碼
+const passwordInput = ref('') // 使用者輸入的密碼
+const message = ref('') // 顯示是否符合的訊息
 
+// 加密密碼並儲存
+const setPassword = () => {
+  const salt = bcrypt.genSaltSync(10) // 產生鹽 (salt)
+  storedHashedPassword.value = bcrypt.hashSync(passwordInput.value, salt) // 將密碼加密
+  message.value = '密碼已加密並儲存！'
+  passwordInput.value = '' // 清空輸入框
+}
 
+// 驗證密碼是否正確
+const checkPassword = () => {
+  const isValid = bcrypt.compareSync(passwordInput.value, storedHashedPassword.value) // 比較密碼
+  message.value = isValid ? '密碼符合！' : '密碼不符合！'
+  passwordInput.value = '' // 清空輸入框
+}
 </script>
 
-
-
 <template>
-
-  <body>
-    <p>I'm Game</p>
-    <form action="/fff">
-      <div>
-        <input type="radio" id="one" name="ccc">
-        <label for="one">check one</label>
-        <input type="radio" id="two" name="ccc">
-        <label for="two">check two</label>
-        <input type="radio" id="three" name="ccc">
-        <label for="three">check three</label>
-      </div>
-      <br>
-      <button>this is a button</button>
-    </form>
-
-    <section>
-      <h1>My name is red</h1>
-      <h2>no red</h2>
-      <div class="ggg">
-        <h1>My name is light blue</h1>
-        <h2>I don't know</h2>
-      </div>
-    </section>
-    <button class="beautiful">Beautiful</button>
-    <div class="container">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
+  <div class="container mt-5">
+    <div class="text-center">
+      <h1 class="mb-4">登入驗證</h1>
+      <p>儲存與檢查密碼範例 (bcrypt.js)</p>
     </div>
 
-    <div id="myDiv">
-      <p>這是第一段。<b>這是粗體文字</b></p>
-      <p>這是第二段。</p>
+    <div class="card mx-auto p-4" style="max-width: 400px;">
+      <div class="mb-3">
+        <label for="password" class="form-label">輸入密碼</label>
+        <input
+          id="password"
+          type="password"
+          class="form-control"
+          v-model="passwordInput"
+          placeholder="輸入密碼"
+        />
+      </div>
+
+      <div class="d-flex justify-content-between">
+        <button class="btn btn-success" @click="setPassword">儲存密碼</button>
+        <button class="btn btn-primary" @click="checkPassword">檢查密碼</button>
+      </div>
+
+      <div class="mt-3">
+        <p class="text-center fw-bold" :class="{'text-success': message === '密碼符合！', 'text-danger': message === '密碼不符合！'}">
+          {{ message }}
+        </p>
+      </div>
     </div>
-  </body>
+    {{ storedHashedPassword }}
+  </div>
 </template>
 
-<style scoped>
-@media (max-width: 1000px) {
-  p {
-    text-align: center;
-    color: rgb(64, 0, 255);
-  }
-}
-
-@media (max-width: 600px) {
-  p {
-    text-align: center;
-    color: red;
-  }
-}
-
-body{
-  transform: 1;
-}
-
-
-h1:hover {
-  color: lightblue;
-  font-weight: bold;
-}
-
-.beautiful {
-  background: none;
-  color: #ffa260;
-  border: 2px solid;
-  padding: 1em 1.5em;
-  font-size: 1.5em;
-  transition: all 0.2s;
-}
-
-.beautiful:hover {
-  border-color: #f1ff5c;
-  color: white;
-  box-shadow: 0 0.5em 1em -0.8em #f1ff5c;
-  transform: translateY(-0.25em);
-}
-
-.container {
-  background-color: white;
-  height: 500px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
-}
-
-.container div {
-  font-size: 5em;
-  text-align: center;
-  width: 200px;
-  height: 200px;
-  flex-basis: 300px;
-  flex-grow: 1;
-}
-
-.container div:nth-of-type(1) {
-  background: lightblue;
-  flex-grow: 2;
-  flex-shrink: 2;
-}
-
-.container div:nth-of-type(2) {
-  background: rgb(190, 230, 173);
-}
-
-.container div:nth-of-type(3) {
-  background: rgb(55, 13, 145);
-}
-
-.container div:nth-of-type(4) {
-  background: rgb(201, 201, 9);
-}
-
-.container div:nth-of-type(5) {
-  background: rgb(240, 93, 1);
+<style>
+body {
+  background-color: #f8f9fa;
 }
 </style>
