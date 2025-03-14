065d3215b2e1f660d0a92cd425433dc30b2ef0b3
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-03-14 19:16:06 +0800

    chore: update chart.js to version 4.4.8 and add path alias in vite.config.js

--- vue3/vite.config.js
+++ vue3/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'  // 需要添加 path 模塊
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')  // 將 @ 符號映射到 src 目錄
+    }
+  }
 })
